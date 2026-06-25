4cb55828f01126d40edd9f5678736e094cbb89cc
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2026-06-25 06:12:20 +0000

    :nit

--- aredis_om/_compat.py
+++ aredis_om/_compat.py
@@ -63,7 +63,7 @@ class ModelField:
 
     def __post_init__(self) -> None:
         self._type_adapter: TypeAdapter[Any] = TypeAdapter(
-            Annotated[self.field_info.annotation, self.field_info]
+            Annotated[self.field_info.annotation, self.field_info]  # type: ignore
         )
 
     def get_default(self) -> Any:
